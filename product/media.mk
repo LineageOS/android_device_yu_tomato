71a92be51eb0b04e1086c7b4e1d7f8d033be208a
Ketut P. Kumajaya <ketut.kumajaya@gmail.com>
2016-11-03 09:27:02 +0100

    tomato: Add missing functions and symbols for IMS
    Change-Id: I84413beb51f921fffed3b2e8d6f8b5b9c3d9b220

--- product/media.mk
+++ product/media.mk
@@ -1,3 +1,7 @@
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
+# Widevine DRM symbol, boringssl-compat subset
+PRODUCT_PACKAGES += \
+    libshim_boringssl
