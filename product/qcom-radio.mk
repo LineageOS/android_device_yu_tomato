7e1bed1aedb182a95389a160cd7c9e8fe2c11dc3
ishantvivek <ishantvivek1311@gmail.com>
2016-11-06 07:51:00 +0100

    tomato: Update ims,radio properties
    Change-Id: I6df843c0247030c14d0bd76ca8dfe2c13f05066a

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -1,12 +1,14 @@
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
     persist.data.target=dpm1 \
     persist.radio.multisim.config=dsds \
     ro.config.always_show_roaming=true \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
     ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
     ril.ecclist1=000,08,100,101,102,110,112,118,119,120,122,911,999 \
-    ro.telephony.default_network=9,9
+    ro.telephony.default_network=9,9 \
+    ril.subscription.types=NV,RUIM
 
 # RIL
 PRODUCT_COPY_FILES += \
