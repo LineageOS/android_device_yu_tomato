48418586d20db1b4d3bc5af0eaf8d750b8993cad
ishantvivek <ishantvivek1311@gmail.com>
2016-11-06 07:51:00 +0100

    tomato: Update ims,radio properties
    Change-Id: I6df843c0247030c14d0bd76ca8dfe2c13f05066a

---
+++ product/ims.mk
@@ -0,0 +1,14 @@
+# IMS properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.lte.volte_call_capable=true \
+    persist.ims.enableADBLogs=1 \
+    persist.ims.enableDebugLogs=1 \
+    persist.radio.calls.on.ims=true \
+    persist.radio.csvt.enabled=false \
+    persist.radio.jbims=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_USE_MDM_TIME=0 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.volte_enabled_by_hw=1
