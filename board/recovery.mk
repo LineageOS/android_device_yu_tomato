46d9d8098c1388661b1b31a2c9bfb869ee747f70
Louis Popi <theh2o64@gmail.com>
2017-05-13 12:09:33 +0200

    tomato: Switch to hdpi for recovery density
    * Recovery image is too big
    
    Change-Id: Ida7215a8707616b04f3820ed6f786b5e513f1973

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,4 +1,4 @@
 # Recovery
-TARGET_RECOVERY_DENSITY := xhdpi
+TARGET_RECOVERY_DENSITY := hdpi
 TARGET_RECOVERY_FSTAB := device/yu/tomato/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
