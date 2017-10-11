ff9fb8925fccedbb4a47e4eaa12ab17829d16cbc
Michael Bestas <mkbestas@lineageos.org>
2017-10-11 19:48:31 +0300

    tomato: Add kernel flags
    Change-Id: I0fbf3f9b48ab7af2050f8b4e6f09dd4294c7a33a

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,3 +1,6 @@
 # Kernel
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
