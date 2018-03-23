b47b7d8afed4270061f13cf042da593a121854c1
Michael Bestas <mkbestas@lineageos.org>
2018-03-23 20:38:40 +0200

    tomato: Use common aw2013 HIDL light HAL
    Change-Id: Ie68cf6d13af5f7d3a9da35bdae2edf6733300269

--- device.mk
+++ device.mk
@@ -86,11 +86,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.msm8916
-
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.aw2013
 
 # Media
 PRODUCT_COPY_FILES += \
